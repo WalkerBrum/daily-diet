2e388563c8d29713b2f1f5efb51cc1e3900b6f4b
My Name <walker.b.lobato@gmail.com>
2023-10-25 00:37:36 -0300

    feat: initial config project

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native"
+import { Container } from "./styles"
+
+export const Home = () => {
+  return (
+    <Container>
+      <Text>Home</Text>
+    </Container>
+  )
+}
---
+++ src/theme/index.ts
@@ -0,0 +1,33 @@
+export default {
+  COLORS: {
+    WHITE: '#FFFFFF',
+
+    GRAY_100: '#FAFAFA',
+    GRAY_200: '#EFF0F0',
+    GRAY_300: '#DDDEDF',
+    GRAY_400: '#B9BBBC',
+    GRAY_500: '#5C6265',
+    GRAY_600: '#333638',
+    GRAY_700: '#1B1D1E',
+
+    GREEN_LIGHT: '#E5F0DB',
+    GREEN_MID: '#CBE4B4',
+    GREEN_DARK: '#639339',
+
+    RED_LIGHT: '#F4E6E7',
+    RED_MID: '#F3BABD',
+    RED_DARK: '#BF3B44',
+  },
+  FONT_FAMILY: {
+    REGULAR: 'NunitoSans_400Regular',
+    BOLD: 'NunitoSans_700Bold',
+  },
+  FONT_SIZE: {
+    FONT_12: 12,
+    FONT_14: 14,
+    FONT_16: 16,
+    FONT_18: 18,
+    FONT_24: 24,
+    FONT_32: 32,
+  }
+}
